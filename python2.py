d7eeb6821a19f5057b0a4682477f5267ad9e9224
Dumitru Simidin <d.simidin@developmentaid.org>
2018-09-06 18:55:52 +0300

    new python files

---
+++ python2.py
@@ -0,0 +1,5 @@
+x = input("Egor introduce primul numar: ")
+y = input("intoduce al doilea numar: ")
+c = x + y
+print('Egor rezultatul este:  ')
+print(c)
---
+++ python3.py
@@ -0,0 +1,7 @@
+x = input("Introduce primul item din lista ")
+y = input("introduce al doilea item din lista: ")
+z = input("introduce al treilea item din lista: ")
+
+lista = [x,y,z]
+for r in lista:
+  print(r)
